30b439e3da931f56f256dcbaf3d90a10359b8445
Masudbot <151044683+Masudbot@users.noreply.github.com>
2023-11-17 08:40:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['917679982058', 'MASUD BOT', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
